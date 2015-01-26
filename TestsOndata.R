f3e5f67c0690052b7ae21b62ac96f8be8426dd26
XtopheB <bontemps@toulouse.inra.fr>
2015-01-26 10:27:29 +0100

    Clean version of Tests

--- TestsOndata.R
+++ TestsOndata.R
@@ -1,6 +1,11 @@
 # Test des différentes fonctions sur nos donnéees
 
 
+rm(list=ls())
+
+setwd("D:/progs/Celine/Special")   
+#setwd("c:/Chris/progs/Celine/Special")   
+
 ##  Testing wintrim 
 # generating the same numbers than in the real dataset
 set.seed(1234)
@@ -38,9 +43,15 @@ length(which(abs(x.u) < quantile(abs(x.u), probs=1-trimtest)))
 
 #  test des fonctions SpecialReg sur nos données 
 
-
 ## Load the dat directly after an estimation in STATA to have same sample 2771 obs. 
 data.all<-read.dta("../Water/data/FinalFile.dta")
+
+# Load data with results (homogenous) !!
+data.all<-read.dta("FinalFileResults1.dta")
+
+# Load data with results (heterogenous) !!
+data.all<-read.dta("FinalFileResultsHet.dta")
+
 nrow(data.all)
 data.work <- data.all
 
@@ -49,7 +60,33 @@ attach(data.work)
 D <- i_tap
 endo <- isatis_health
 iv <- cbind(itap_2008 ,iconcernwatpol_2008)
-exo <- cbind(i_under18, log_income, i_town, i_car, b08_locenv_water, a2_age, i_can, i_fra)
+exo <- cbind( a2_age,i_under18, log_income, i_town, i_car, b08_locenv_water, i_can, i_fra)
+hetv <- cbind(i_under18, log_income, i_town ,b08_locenv_water)
+
+## Variables def pour test pas a pas TO REMOVE !!!
+# y <- i_tap
+# v <- Special
+# trimtype <-"TRIM"
+# trimlevel <- 0.025
+
+# nouvelle formulation avec exo, endo et iv explicites !!! 
+retour <- specialreg.fitN(D,Special, endo,  exo, iv,  
+                          trimtype="TRIM", trimlevel=0.025 )  
+
+### Test avec hetero !!!
+
+retour2 <- specialreg.fitN(D,Special, endo,  exo, iv,  
+                          trimtype="TRIM", trimlevel=0.025,
+                          hetero = "HETERO", hetv =hetv )  
+
+# Stata bw for homo = 0.07154983, Hetero = .23179664 
+retour3 <- specialreg.fitN(D,Special, endo,  exo, iv,  
+                           trimtype="TRIM", trimlevel=0.005,
+                           hetero = "HETERO", hetv =hetv, 
+                           udensmethod = "CV")  
+
+
+################################## OLD STUFF (Usefull though !) ###############)
 
 # On ajoute les X exo aux instruments
 
@@ -60,12 +97,6 @@ Newiv <- cbind(iv, exo)
 # Test de la fonction complète specialreg.fit 
 
 retour <- specialreg.fit(D,Special, NewX, Newiv,  trimtype="TRIM", trimlevel=0.025 )  
-    # <- bizarre dès la prmeière regression
+# <- bizarre dès la prmeière regression
 
 lm(formula = Special ~ NewX + Newiv)
-
-
-# nouvelle formulation avec exo, endo et iv explicites !!! 
-retour <- specialreg.fitN(D,Special, endo,  exo, iv,  trimtype="TRIM", trimlevel=0.025 )  
-
-
